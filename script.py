5ee0572ff0a48253462eec8166902774a5245d5b
jolegrand10 <georges.georgoulis@wanadoo.fr>
2020-04-28 10:51:09 +0200

    PEP8 compliance checked

--- script.py
+++ script.py
@@ -28,64 +28,64 @@ class DoubleLog():
     """
         setup  full logging in a single call using a kind-of Façade pattern
     """
+
     def __init__(self, filename):
-        logger=logging.getLogger()
+        logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
         # log file
-        file_formatter=logging.Formatter('%(message)s')
+        file_formatter = logging.Formatter('%(message)s')
         file_handler = logging.FileHandler(filename, mode='w')
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(file_formatter)
         logger.addHandler(file_handler)
         # console
-        stream_handler= logging.StreamHandler()
+        stream_handler = logging.StreamHandler()
         stream_handler.setLevel(logging.DEBUG)
         stream_formatter = logging.Formatter('%(message)s', "%H:%M:%S")
         stream_handler.setFormatter(stream_formatter)
         logger.addHandler(stream_handler)
-#class DoubleLog
+# class DoubleLog
 
 
-
-
-d = {} # a global dict to remember already collected info
+d = {}  # a global dict to remember already collected info
 
 
 def remember(**kwargs):
     """update the dict with the collected info pieces"""
-    for k,v in kwargs.items():
-        d[k]=v
+    for k, v in kwargs.items():
+        d[k] = v
 
 
 def checkPlatform():
     logging.info("\n\n -- Platform --\n")
     try:
-        remember(_os = os.name)
+        remember(_os=os.name)
     except Exception as e:
-        remember(_os = "Unknown-os")
+        remember(_os="Unknown-os")
         logging.error(e)
 
     try:
-        remember(_system = platform.system())
+        remember(_system=platform.system())
     except Exception as e:
-        remember(_system = "Unknown-system")
+        remember(_system="Unknown-system")
         logging.error(e)
 
     try:
-        remember(_release = platform.release())
+        remember(_release=platform.release())
     except Exception as e:
-        remember(_release = "Unknown-release")
-        logging.error(e)   
-    
-    try:
-        remember(_homepath = os.path.expanduser("~"))
-    except Exception as e:
-        remember(_homepath = "Unknown-homepath")
+        remember(_release="Unknown-release")
         logging.error(e)
 
+    try:
+        remember(_homepath=os.path.expanduser("~"))
+    except Exception as e:
+        remember(_homepath="Unknown-homepath")
+        logging.error(e)
 
-    logging.info("OS: %s %s, aka \"%s\""%(d["_system"], d["_release"], d["_os"]))
-    logging.info("Home path: %s"%(d["_homepath"]))
+    logging.info("OS: %s %s, aka \"%s\"" %
+                 (d["_system"], d["_release"], d["_os"]))
+    logging.info("Home path: %s" % (d["_homepath"]))
+
 
 def checkPath(p):
     """
@@ -94,27 +94,29 @@ def checkPath(p):
         these words
     """
     def isRelated(w):
-        ref=p.upper().split(" ")
+        ref = p.upper().split(" ")
         for r in ref:
             return r in w.upper()
     try:
-        l = [w for w in filter(isRelated,os.getenv("PATH","").split(";"))]
+        l = [w for w in filter(isRelated, os.getenv("PATH", "").split(";"))]
     except Exception:
         l = []
     if l:
-        s  = "PATH includes (%s)-related values:\n "%(p)
-        s +=("\n ").join(l)
+        s = "PATH includes (%s)-related values:\n " % (p)
+        s += ("\n ").join(l)
         logging.info(s)
 
+
 def getPythonPath():
-    exe = "python.exe" if d["_system"]=="Windows" else "python"
+    exe = "python.exe" if d["_system"] == "Windows" else "python"
     try:
         ep = find_executable("python")
     except Exception:
-        ep= None
+        ep = None
     if ep is not None:
-        return ep.replace(exe,"")
+        return ep.replace(exe, "")
     return ""
+
 
 def checkProg(p):
     """
@@ -129,12 +131,13 @@ def checkProg(p):
     try:
         ep = find_executable(p)
     except Exception:
-        ep= None
+        ep = None
     if wp is not None:
-        logging.info("Command %s is available at:\n %s"%(p,wp))
+        logging.info("Command %s is available at:\n %s" % (p, wp))
     if ep is not None:
-        logging.info("%s is executable from:\n %s"%(p,ep))
+        logging.info("%s is executable from:\n %s" % (p, ep))
     return not(wp is None and ep is None)
+
 
 def runProg(*args):
     """
@@ -144,52 +147,57 @@ def runProg(*args):
         ret is 0 for normal and non 0 for errors
     """
     try:
-        ret = subprocess.run(args, stdout=subprocess.PIPE,\
-         encoding='utf-8')
-        logging.info("\"%s\" :\n %s"%(" ".join(args), ret.stdout[:72]+"..."))
+        ret = subprocess.run(args, stdout=subprocess.PIPE,
+                             encoding='utf-8')
+        logging.info("\"%s\" :\n %s" % (" ".join(args), ret.stdout[:72]+"..."))
         return ret.returncode
     except Exception as e:
-        return 1  
+        return 1
+
 
 def checkPython():
     logging.info("\n\n -- Python --\n")
     try:
-        remember(_pythonVersion = sys.version_info)
-        logging.info("Python version %s.%s.%s"%(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
+        remember(_pythonVersion=sys.version_info)
+        logging.info("Python version %s.%s.%s" % (
+            sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
         if sys.version_info[0] < 3:
             logging.info("You are using an obsolete version of Python")
             logging.info("Use Python 3 !")
     except Exception as e:
-        remember(_PythonVersion = "Unknown-version")
+        remember(_PythonVersion="Unknown-version")
         logging.error(e)
-    
+
     for p in "py python python2 python3".split(" "):
         checkProg(p)
 
     checkPath("python py")
 
+
 def checkPip():
     logging.info("\n\n -- Pip --\n")
     for p in "pip pip2 pip3".split(" "):
         if checkProg(p):
-            r = runProg(p,"--version")
+            r = runProg(p, "--version")
+
 
 def checkJupyter():
     logging.info("\n\n -- Jupyter --\n")
-    ed= d.get("_envFolder","")
+    ed = d.get("_envFolder", "")
     if ed == "":
         return
     for w in ["jupenv", "envjup", "jup", "jupyter"]:
-        f= os.path.join(ed, w)
+        f = os.path.join(ed, w)
         if os.path.isdir(f):
-            logging.info("Jupyter is installed at:\n %s"%(f))
-            remember(_jupenv = f)
+            logging.info("Jupyter is installed at:\n %s" % (f))
+            remember(_jupenv=f)
             break
+
 
 def checkVenv():
     logging.info("\n\n -- Venv --\n")
     for py in "python python3 python2".split(" "):
-        r = runProg(py, "-m","venv", "-h")
+        r = runProg(py, "-m", "venv", "-h")
         if r == 0:
             break
     if r:
@@ -197,21 +205,22 @@ def checkVenv():
 
     hd = d["_homepath"]
     for w in ["environments", "environnements", "environment", "environnement"]:
-        f= os.path.join(hd, w)
+        f = os.path.join(hd, w)
         if os.path.isdir(f):
-            logging.info("Environnement folder is: %s"%(f))
-            remember(_envFolder = f)
+            logging.info("Environnement folder is: %s" % (f))
+            remember(_envFolder=f)
             break
+
 
 def checkIdle():
     logging.info("\n\n -- Idle --\n")
-    if d["_system"]=="Windows":
+    if d["_system"] == "Windows":
         #
         # Python Path + Lib/idlelib/idle.pyw
         #
         first = True
         pp = getPythonPath()
-        idledir= os.path.join(pp, "Lib", "idlelib")
+        idledir = os.path.join(pp, "Lib", "idlelib")
         if os.path.isdir(idledir):
             for w in "idle.py idle.pyw".split(" "):
                 f = os.path.join(idledir, w)
@@ -219,21 +228,23 @@ def checkIdle():
                     if first:
                         logging.info("Found at:")
                         first = False
-                    logging.info("%s"%(f))
+                    logging.info("%s" % (f))
     else:
         checkProg("idle")
-        
+
+
 def checkNppp():
     logging.info("\n\n -- Notepad++ --\n")
     f = "C:\\Program Files (x86)\\Notepad++\\notepad++.exe"
     if os.path.isfile(f):
-        logging.info("Found at:\n %s"%(f))
+        logging.info("Found at:\n %s" % (f))
     else:
         logging.error("Could not find notepad++ in its usual location")
-    
+
+
 def checkPycharm():
     logging.info("\n\n -- PyCharm --\n")
-    if d["_system"]=="Windows":
+    if d["_system"] == "Windows":
         pcdir = "C:\\Program Files\\JetBrains\\"
         if os.path.isdir(pcdir):
             #
@@ -241,47 +252,50 @@ def checkPycharm():
             #
             for f in os.listdir(pcdir):
                 if f.startswith("PyCharm Community Edition 20"):
-                   g = os.path.join(pcdir,f)
-                   if os.path.isdir(g):
-                       logging.info("Found at:\n %s"%(g))
-                   else:
-                       logging.error("Could not find Pycharm")  
+                    g = os.path.join(pcdir, f)
+                    if os.path.isdir(g):
+                        logging.info("Found at:\n %s" % (g))
+                    else:
+                        logging.error("Could not find Pycharm")
                 else:
                     logging.error("Could not find Pycharm")
         else:
             logging.error("Could not find Pycharm")
     else:
-        logging.info("Cannot check PyCharm installation @ %s"%(d["_system"]))
-                     
+        logging.info("Cannot check PyCharm installation @ %s" % (d["_system"]))
+
+
 def main():
     #
     # Parse cmd line
     #
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dump",
-                        action ='store_true',
+                        action='store_true',
                         help="dumps the dictionary at the end of the report")
     args = parser.parse_args()
     #
     # collect minimal info, to setup log file
     #
-    remember(_isodate = datetime.datetime.now().strftime("%Y-%m-%d"))
-    remember(_timestamp="%s"%(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))    
+    remember(_isodate=datetime.datetime.now().strftime("%Y-%m-%d"))
+    remember(_timestamp="%s" %
+             (datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
     try:
-        remember(_username = getpass.getuser())
+        remember(_username=getpass.getuser())
     except Exception:
-        remember(_username = "Unknown-user")
-    
+        remember(_username="Unknown-user")
+
     try:
-        remember (_hostname = socket.gethostname())
+        remember(_hostname=socket.gethostname())
     except Exception:
-        remember(_username = "Unknown-host")
-    remember(_logFile ="%s@%s.log"%(d["_username"],d["_hostname"]))
+        remember(_username="Unknown-host")
+    remember(_logFile="%s@%s.log" % (d["_username"], d["_hostname"]))
     DoubleLog(d['_logFile'])
     #
     # initial message
     #
-    s="Analysis of %s@%s started at %s"%(d["_username"],d["_hostname"], d["_timestamp"])
+    s = "Analysis of %s@%s started at %s" % (
+        d["_username"], d["_hostname"], d["_timestamp"])
     logging.info(s)
     #
     # show basic platform info
@@ -306,7 +320,7 @@ def main():
     #
     # On windows only, check Notepad++
     #
-    if d["_system"]=="Windows":
+    if d["_system"] == "Windows":
         checkNppp()
     #
     #
@@ -319,8 +333,8 @@ def main():
     #
     # final message
     #
-    s="\n\nAnalysis of %s@%s completed at %s"%(d["_username"],d["_hostname"],
-        "%s"%(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
+    s = "\n\nAnalysis of %s@%s completed at %s" % (d["_username"], d["_hostname"],
+                                                   "%s" % (datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
     logging.info(s)
     #
     #
@@ -328,9 +342,8 @@ def main():
     if args.dump:
         logging.info("\n")
         for k, v in sorted(d.items()):
-            logging.info("%s --> %s"%(k,v))
+            logging.info("%s --> %s" % (k, v))
 
 
-if __name__=="__main__":
-	main()
-    
+if __name__ == "__main__":
+    main()
